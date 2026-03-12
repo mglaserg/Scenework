041619c823d10935f6dd2726e07ea4b784864751
mglaserg <mglaserg@gmail.com>
2026-03-12 13:14:39 -0400

    update ssl connection require: true part 2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url,
-    ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: false, require: true },
   },
 });
